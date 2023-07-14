44b8def0d4f0fd53061b876b3592fe2c5baa536e
Wael Konsowa <wkbeda@gmail.com>
2023-07-13 17:14:11 -0700

    task 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Program print with put function
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
