fbd2db86233e2b8fe8ef11cc04a55677f2537231
Wael Konsowa <wkbeda@gmail.com>
2023-07-13 17:44:28 -0700

    task 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print aquote
+ *
+ * Return: This time we return an error 1
+ */
+
+int main(void)
+{
+	char quo[] = "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
