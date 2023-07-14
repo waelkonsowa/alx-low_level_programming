274b5ea4f3c1c3577ed04cdab8407f73c2ce5f2f
Wael Konsowa <wkbeda@gmail.com>
2023-07-13 17:22:30 -0700

    task 1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints with printf
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
