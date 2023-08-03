9585d557bb619c8a23b4034ad97bbc78880e96c4
Wael Konsowa <wkbeda@gmail.com>
2023-08-02 17:51:50 -0700

    task

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,6 +1,3 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -8,5 +5,12 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-
-#endif /* HOLBERTON_H */
+int halp(int c, int i);
+int is_prime_number(int n);
+int hai(int n, int d);
+int _sqrt(int x, int i);
+int is_palindrome(char *s);
+int get_length(char *s);
+int my_pal(char *s, int l);
+int wildcmp(char *s1, char *s2);
+int findsrc(char *s, char c, int i, int p);
